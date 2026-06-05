e663c89edc35ccb16ee9f7a5286cd1c276b4a053
Indrajit Gupta <indrajitgupta147@gmail.com>
2026-06-06 02:07:20 +0600

    Create separate data project
    App.data  19

--- Adda.Data/Models/Post.cs
+++ Adda.Data/Models/Post.cs
@@ -5,7 +5,7 @@ namespace Adda.Data.Models
     public class Post
     {
         [Key] 
-        public int PostId { get; set; }
+        public int Id { get; set; }
         public string Content { get; set; }
         public string? ImageUrl { get; set; }
         public int NrOfReports { get; set; }
